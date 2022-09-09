73c5eadd3f75ce77dd0200bd318ca23327d88d6e
Ahmed Hanafy <ahmedhanafyr99@gmail.com>
2022-09-08 19:35:22 -0700

    Finished linked list - solo experiment

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,39 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.length = 0;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    list.length++
+    if(list.head === null) {
+      list.head = newNode;
+      list.tail = list.head;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    if(list.head !== null) {
+      var removedHeadValue = list.head.value;
+      list.head = list.head.next
+      list.length--;
+    }
+    return removedHeadValue;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while (currentNode !== null) {
+      if (currentNode.value !== target) {
+        currentNode = currentNode.next;
+      } else {
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
