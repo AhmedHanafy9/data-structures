5c97f9c1c6d7c4451861d29072880b0eb58e3d0b
Ahmed Hanafy <ahmedhanafyr99@gmail.com>
2022-09-10 12:39:30 -0700

    Sprint complete, all tests passing

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,58 @@
 var BinarySearchTree = function(value) {
+  var binarySearchTree = {}
+  binarySearchTree.value = value;
+  binarySearchTree.left = null;
+  binarySearchTree.right = null;
+
+  _.extend(binarySearchTree, binarySearchTreeMethods);
+  return binarySearchTree;
 };
+
+var binarySearchTreeMethods = {};
+
+binarySearchTreeMethods.insert = function(value) {
+ var newChild = BinarySearchTree(value);
+
+ if (newChild.value < this.value) {
+  if (this.left === null) {
+    this.left = newChild;
+    return;
+  } else {
+    this.left.insert(value)
+  }
+ } else if (newChild.value > this.value) {
+  if (this.right === null) {
+    this.right = newChild;
+    return;
+  } else {
+    this.right.insert(value)
+  }
+ }
+};
+
+binarySearchTreeMethods.contains = function(value) {
+  var found = false;
+  if (this.value === value) {
+    found = true;
+    return found;
+  } else if (this.left !== null && this.value > value) {
+    return this.left.contains(value);
+  } else if (this.right !== null && this.value < value) {
+    return this.right.contains(value);
+  }
+  return found;
+};
+
+
+binarySearchTreeMethods.depthFirstLog = function(cb) {
+
+}
+
+
+
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
